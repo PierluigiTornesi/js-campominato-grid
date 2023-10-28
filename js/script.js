40855bd35d170590a242b8b8fc97172cdd476dfd
PierluigiTornesi <pierluigi.tornesi02@gmail.com>
2023-10-28 13:33:53 +0200

    add: aggiunta scelta livello e bonus

--- js/script.js
+++ js/script.js
@@ -2,22 +2,69 @@
 //genero l'azione sul bottone quando viene premuto
 const buttonPlay = document.getElementById("play");
 buttonPlay.addEventListener("click", function() {
-    //prendo l'elemento con la classe grid per poter aggiungere le celle
-    const gridElem = document.querySelector(".grid");
+    //prendo la scelta del livello di difficoltá
+    const sceltaDiff = document.getElementById("livelloDif")
+    //prendo il valore del livello
+    const diffLiv = parseInt(sceltaDiff.value);
+    console.log(diffLiv);
+    //verifico il valore del livello
+    //se é uno creo una griglia con 100 celle
+    if (diffLiv === 1) {
+        //prendo l'elemento con la classe grid per poter aggiungere le celle
+        const gridElem = document.querySelector(".grid");
 
-    //ciclo per creare 100 celle
-    for (let i = 0; i < 100; i++) {
-        //il numero corrente é l'indice piú uno cosi da scrivere i numeri da 1 a 100
-        const curNumber = i + 1;
-        //generare la cella della griglia
-        const cell = generateGridCell(curNumber) //cella é elemento html, quindi posso aggiungergli event listener
-        cell.addEventListener("click",handleCellClick)
-    
-    
-        //debug
-        //console.log(cell);
-        gridElem.append(cell);
-    }  
+        //ciclo per creare 100 celle
+        for (let i = 0; i < 100; i++) {
+            //il numero corrente é l'indice piú uno cosi da scrivere i numeri da 1 a 100
+            const curNumber = i + 1;
+            //generare la cella della griglia
+            const cell = generateGridCell(curNumber,diffLiv) //cella é elemento html, quindi posso aggiungergli event listener
+            cell.addEventListener("click",handleCellClick)
+
+            //debug
+            //console.log(cell);
+            //inserisco le celle nell'html
+            gridElem.append(cell);
+        } 
+    } else 
+        //se il livello scelto é due creo una griglia con 81 celle
+        if(diffLiv === 2) {
+        //prendo l'elemento con la classe grid per poter aggiungere le celle
+        const gridElem = document.querySelector(".grid");
+
+        //ciclo per creare 81 celle
+        for (let i = 0; i < 81; i++) {
+            //il numero corrente é l'indice piú uno cosi da scrivere i numeri da 1 a 81
+            const curNumber = i + 1;
+            //generare la cella della griglia
+            const cell = generateGridCell(curNumber,diffLiv) //cella é elemento html, quindi posso aggiungergli event listener
+            cell.addEventListener("click",handleCellClick)
+
+            //debug
+            //console.log(cell);
+            //inserisco le celle nell'html
+            gridElem.append(cell);
+        } 
+    }else 
+        //se il livello scelto é tre creo una griglia con 49 celle
+        if(diffLiv === 3){
+        //prendo l'elemento con la classe grid per poter aggiungere le celle
+        const gridElem = document.querySelector(".grid");
+
+        //ciclo per creare 49 celle
+        for (let i = 0; i < 49; i++) {
+            //il numero corrente é l'indice piú uno cosi da scrivere i numeri da 1 a 49
+            const curNumber = i + 1;
+            //generare la cella della griglia
+            const cell = generateGridCell(curNumber,diffLiv) //cella é elemento html, quindi posso aggiungergli event listener
+            cell.addEventListener("click",handleCellClick)
+
+            //debug
+            //console.log(cell);
+            //inserisco le celle nell'html
+            gridElem.append(cell);
+        } 
+    }
 })
 
 
@@ -31,9 +78,15 @@ buttonPlay.addEventListener("click", function() {
  * @param {number} innerNumber numero da inserire nella cella
  * @returns {any} elemento html della cella
  */
-function generateGridCell(innerNumber) {
+function generateGridCell(innerNumber,numLiv) {
     const newCell = document.createElement("div");
-    newCell.classList.add("cell");
+    if(numLiv === 1){
+        newCell.classList.add("cell-10");
+    }else if(numLiv === 2){
+        newCell.classList.add("cell-9");
+    }else if(numLiv === 3){
+        newCell.classList.add("cell-7");
+    }   
     newCell.innerHTML = innerNumber;
     return newCell;
 }
